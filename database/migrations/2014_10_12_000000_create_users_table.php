96ba07356ce274d0f3adb69ff83d4810e7ea1d59
Alex Simonson <shaggy7569@gmail.com>
2019-07-25 16:54:25 -0400

    query for users that you haven't matched with yet

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,7 +20,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->unsignedInteger('age');
+            $table->unsignedInteger('age')->nullable();
             $table->string('image')->nullable();
             $table->rememberToken();
             $table->timestamps();
--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -2,5 +2,39 @@
 @section('title', 'Search')
 
 @section('content')
+@php
+$userAuth = Auth::user();
+$allUsers = App\User::all();
+$matches = collect();
+$ulfAuth = $userAuth->usersLikedFirst;
+$ulsAuth = $userAuth->usersLikedSecond;
+foreach($ulfAuth as $ul){
+    $matches->add($ul);
+}
+foreach($ulsAuth as $ul){
+    $matches->add($ul);
+}
+
+$removeUsers = array();
+//add authenticated user to ignore array
+$removeUsers[] = $userAuth->id;
+foreach($matches as $match){
+    $removeUsers[] = $match->id;
+}
+//query users not in the list we made
+$potentialUsers = App\User::whereNotIn('id', $removeUsers)->get();
+
+@endphp
+
+@foreach ($potentialUsers as $user)
+    <card-frame>
+        <div slot='header'>
+            <h2>Profile</h2>
+        </div>
+        <div slot='body'>
+            <search-display-card id='{{$user->id}}' name='{{$user->name}}' bio='{{$user->bio}}' age='{{$user->age}}' image='{{$user->image}}'></search-display-card>
+        </div>
+    </card-frame>
+@endforeach
 
 @endsection
